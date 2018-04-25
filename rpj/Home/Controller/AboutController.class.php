8977dbd63185fe77fc3173719a7a6bbb9897f642
chikwongyip <527439024@qq.com>
2018-04-25 21:35:38 +0800

    20180425
    20180425

--- rpj/Home/Controller/AboutController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class AboutController extends Controller {
-    public function about(){
-        $this->display();
-    }
-}
--- rpj/Home/Controller/ContractController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class ContractController extends Controller {
-    public function Contract(){
-        $this->display();
-    }
-}
